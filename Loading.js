859a42b92b0a54f38da61fd98c4fbb099f3e375b
coffeemori92 <coffeemori92@naver.com>
2020-01-15 22:10:34 +0900

    screen

---
+++ Loading.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function Loading(){
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Getting the Weather</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingHorizontal: 20,
+        paddingVertical: 100,
+        backgroundColor: '#FDF6AA'
+    },
+    text: {
+        color: '#2c2c2c',
+        fontSize: 25
+    }
+});
+
+export default Loading;
